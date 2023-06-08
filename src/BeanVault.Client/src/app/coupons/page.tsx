a76fcf51f96f2f0bccaf3f980b04b22e3ad33c88
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-06-07 22:18:42 -0500

    feat: add coupons index view

--- src/BeanVault.Client/src/app/coupons/page.tsx
+++ src/BeanVault.Client/src/app/coupons/page.tsx
@@ -1,3 +1,62 @@
-export default function CouponsPage() {
-  return <h1>Coupons</h1>;
+import { fetchClient } from '@/http/fetchClient';
+import { couponService } from '@/services/couponService';
+import { AiOutlinePlusSquare } from 'react-icons/ai';
+import { BsFillTrash3Fill } from 'react-icons/bs';
+import styles from './Page.module.css';
+
+export default async function CouponsPage() {
+  const { getCoupons } = couponService({ client: fetchClient() });
+  const coupons = await getCoupons();
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
+
+  return (
+    <div className={styles.card}>
+      <div className={styles.cardHeader}>
+        <h1>Coupons List</h1>
+      </div>
+      <div className={styles.cardBody}>
+        <div className={styles.addButtonContainer}>
+          <button
+            title="Add New Coupon"
+            type="button"
+            className={styles.addCouponButton}
+          >
+            <AiOutlinePlusSquare />
+            Create New Coupon
+          </button>
+        </div>
+        <table className={styles.couponTable}>
+          <thead className={styles.couponTableHeader}>
+            <tr>
+              <th>Coupon Code</th>
+              <th>Discount Amount</th>
+              <th>Minimum Amount</th>
+            </tr>
+          </thead>
+          <tbody className={styles.couponTableBody}>
+            {coupons.map(coupon => (
+              <tr>
+                <td>{coupon.couponCode}</td>
+                <td>{formatter.format(coupon.discountAmount)}</td>
+                <td>{formatter.format(coupon.minAmount)}</td>
+                <td>
+                  <button
+                    data-coupon-id={coupon.id}
+                    title="delete coupon button"
+                    type="button"
+                    className={styles.deleteCouponButton}
+                  >
+                    <BsFillTrash3Fill />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
--- src/BeanVault.Client/src/services/couponService.ts
+++ src/BeanVault.Client/src/services/couponService.ts
@@ -1,10 +1,23 @@
 import { fetchClient } from '@/types/fetchClient.js';
 
+type Coupon = {
+  id: string;
+  couponCode: string;
+  discountAmount: number;
+  minAmount: number;
+};
+
 export function couponService({ client }: { client: fetchClient }) {
   const COUPON_SERVICE_URL = process.env.NEXT_PUBLIC_COUPON_SERVICE_URL;
 
-  async function getCoupons() {
-    return await client.get(`${COUPON_SERVICE_URL}/api/coupons`);
+  async function getCoupons(): Promise<Coupon[]> {
+    const res = await client.get(`${COUPON_SERVICE_URL}/api/coupons`);
+
+    if (res.ok === false) {
+      throw new Error('Unable to get coupons');
+    }
+
+    return await res.json();
   }
 
   return {
