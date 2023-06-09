bc2fde635bb9e024c7e2d0eb793d913bccfa72e3
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-06-09 11:32:09 -0500

    feat: create add coupon state and derive form fields from that state

--- src/BeanVault.Client/src/components/AddCouponForm.tsx
+++ src/BeanVault.Client/src/components/AddCouponForm.tsx
@@ -1,53 +1,110 @@
+'use client';
+
 import Link from 'next/link';
+import { HTMLInputTypeAttribute, InputHTMLAttributes, useReducer } from 'react';
 import styles from './AddCouponForm.module.css';
 
 export default function AddCouponForm() {
+  type FormAction =
+    | { type: 'updateValue'; payload: { name: string; value: string } }
+    | { type: 'updateErrors' };
+
+  type FormState = { [key: string]: FormField };
+
+  type FormField = {
+    labelText: string;
+    type: HTMLInputTypeAttribute;
+    value: string;
+    errors: string[];
+    inputProps?: InputHTMLAttributes<HTMLInputElement>;
+  };
+
+  const initialFormState: FormState = {
+    couponCode: {
+      labelText: 'Coupon Code',
+      type: 'text',
+      value: '',
+      errors: [],
+      inputProps: {
+        placeholder: 'BuyOneGetOne',
+      },
+    },
+    discountAmount: {
+      labelText: 'Discount Amount',
+      type: 'number',
+      value: '',
+      errors: [],
+      inputProps: {
+        placeholder: '1',
+        min: '1',
+      },
+    },
+    minAmount: {
+      labelText: 'Minimum Amount',
+      type: 'number',
+      value: '',
+      errors: [],
+      inputProps: {
+        placeholder: '0',
+        min: '0',
+      },
+    },
+  };
+
+  function formReducer(state: FormState, action: FormAction) {
+    switch (action.type) {
+      case 'updateValue': {
+        const field = state[action.payload.name];
+        const newField = { ...field };
+        newField.value = action.payload.value;
+        return {
+          ...state,
+          [action.payload.name]: newField,
+        };
+      }
+      case 'updateErrors':
+        return state;
+      default:
+        return state;
+    }
+  }
+
+  const [formState, dispatch] = useReducer(formReducer, initialFormState);
+
   return (
     <div className={styles.container}>
       <h1 className={styles.formHeader}>Add Coupon</h1>
       <hr className={styles.separator} />
       <form className={styles.addCouponForm}>
-        <div className={styles.formGroup}>
-          <label className={styles.formLabel} htmlFor="couponCode">
-            Coupon Code
-          </label>
-          <input
-            placeholder="BuyOneGetOne"
-            id="couponCode"
-            name="couponCode"
-            className={styles.formControl}
-            type="text"
-          />
-          <span className={styles.error}></span>
-        </div>
-        <div className={styles.formGroup}>
-          <label className={styles.formLabel} htmlFor="discountAmount">
-            Discount Amount ($)
-          </label>
-          <input
-            placeholder="1"
-            id="discountAmount"
-            name="discountAmount"
-            className={styles.formControl}
-            type="number"
-            min="1"
-          />
-          <span className={styles.error}></span>
-        </div>
-        <div className={styles.formGroup}>
-          <label className={styles.formLabel} htmlFor="minAmount">
-            Minimum Amount ($)
-          </label>
-          <input
-            placeholder="1"
-            id="minAmount"
-            name="minAmount"
-            className={styles.formControl}
-            type="number"
-            min="1"
-          />
-          <span className={styles.error}></span>
-        </div>
+        {Object.keys(formState).map(key => {
+          const formField = formState[key];
+          return (
+            <div key={key} className={styles.formGroup}>
+              <label className={styles.formLabel} htmlFor={key}>
+                {formField.labelText}
+              </label>
+              <input
+                {...formField.inputProps}
+                id={key}
+                name={key}
+                className={styles.formControl}
+                type={formField.type}
+                value={formField.value}
+                onChange={e =>
+                  dispatch({
+                    type: 'updateValue',
+                    payload: { name: e.target.name, value: e.target.value },
+                  })
+                }
+              />
+              <ul className={styles.error}>
+                {formField.errors.map(error => (
+                  <li>{error}</li>
+                ))}
+              </ul>
+            </div>
+          );
+        })}
         <div className={styles.actionContainer}>
           <Link className={styles.cancelButton} href="coupons">
             Cancel
