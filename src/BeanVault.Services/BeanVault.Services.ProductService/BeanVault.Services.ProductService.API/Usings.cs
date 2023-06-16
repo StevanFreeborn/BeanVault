47656688eca16e64d0630853735a584d82aa611a
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-06-16 11:30:59 -0500

    chore: run dotnet format

--- src/BeanVault.Services/BeanVault.Services.ProductService/BeanVault.Services.ProductService.API/Usings.cs
+++ src/BeanVault.Services/BeanVault.Services.ProductService/BeanVault.Services.ProductService.API/Usings.cs
@@ -4,10 +4,10 @@ global using System.Text;
 global using System.Text.Json;
 
 global using BeanVault.Services.ProductService.API.Dtos;
+global using BeanVault.Services.ProductService.API.Extensions;
 global using BeanVault.Services.ProductService.API.Middleware;
 global using BeanVault.Services.ProductService.API.Models;
 global using BeanVault.Services.ProductService.Core.Exceptions;
-global using BeanVault.Services.ProductService.API.Extensions;
 global using BeanVault.Services.ProductService.Core.Interfaces;
 global using BeanVault.Services.ProductService.Core.Models;
 global using BeanVault.Services.ProductService.Infrastructure.DependencyInjection;
